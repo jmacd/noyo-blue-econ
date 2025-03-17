d94e90e9af53cefc4d36ebe8ab7430ad0595b7f4
Joshua MacDonald <josh.macdonald@gmail.com>
2025-03-16 22:12:54 -0700

    cfg

--- init.sh
+++ init.sh
@@ -4,28 +4,28 @@ export RUST_BACKTRACE=1
 
 DIR=$(pwd)
 INBOX=${DIR}/history
-EXE="docker run -e POND=/mnt/POND -v ${HOME}:/mnt duckpond:latest"
+EXE="docker run -e POND=/pond/POND -v ${HOME}:/pond ${DIR}:/src duckpond:latest"
 
 echo ---- init
 ${EXE} init || exit 1
 
 echo ---- apply inbox
-${EXE} apply -f ${DIR}/config/inbox.yaml || exit 1
+${EXE} apply -f /src/config/inbox.yaml || exit 1
 
 echo ---- apply laketech
-${EXE} apply -f ${DIR}/config/laketech.yaml || exit 1
+${EXE} apply -f /src/config/laketech.yaml || exit 1
 
 echo ---- apply hydrovu
-${EXE} apply -f ${DIR}/config/noyo.yaml -v "key=${HYDROVU_CLIENT_ID}" -v "secret=${HYDROVU_CLIENT_SECRET}" || exit 
+${EXE} apply -f /src/config/noyo.yaml -v "key=${HYDROVU_CLIENT_ID}" -v "secret=${HYDROVU_CLIENT_SECRET}" || exit 
 
 echo ---- apply noyodata
-${EXE} apply -f ${DIR}/config/noyodata.yaml || exit 1
+${EXE} apply -f /src/config/noyodata.yaml || exit 1
 
 echo ---- apply reduce
-${EXE} apply -f ${DIR}/config/reduce.yaml || exit 1
+${EXE} apply -f /src/config/reduce.yaml || exit 1
 
 echo ---- apply observable
-${EXE} apply -f ${DIR}/config/observe.yaml || exit 1
+${EXE} apply -f /src/config/observe.yaml || exit 1
 
 #echo ---- run
 #${EXE} run || exit 1
