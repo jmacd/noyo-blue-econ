47a07708468a857be98d6a57a1fec149dce6ec33
Joshua MacDonald <josh.macdonald@gmail.com>
2025-12-19 19:51:04 -0800

    again

--- scripts/pond.sh
+++ scripts/pond.sh
@@ -9,9 +9,17 @@ OUTDIR=${ROOT}/src/data
 CONFIG=${ROOT}/config
 POND=${ROOT}/pond
 
+
+
+
 podman run -ti --rm \
        -v "${POND}:/pond" \
        -v "${OUTDIR}:/data" \
        -v "${CONFIG}:/config" \
        -e POND=/pond \
+       -e HYDRO_KEY_ID=${HYDRO_KEY_ID} \
+       -e HYDRO_KEY_VALUE=${HYDRO_KEY_VALUE} \
+       -e R2_ENDPOINT=${R2_ENDPOINT} \
+       -e R2_KEY=${R2_KEY} \
+       -e R2_SECRET=${R2_SECRET} \
        "${IMAGE}" "$@"
