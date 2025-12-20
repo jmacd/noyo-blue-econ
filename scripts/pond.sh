55a109bbb5838f9b176d20c6ae328fccadf84208
Joshua MacDonald <josh.macdonald@gmail.com>
2025-12-19 19:46:29 -0800

    again

--- scripts/pond.sh
+++ scripts/pond.sh
@@ -6,10 +6,12 @@ SCRIPTS=$(cd "$(dirname "$0")" && pwd)
 ROOT=$(dirname "${SCRIPTS}")
 
 OUTDIR=${ROOT}/src/data
+CONFIG=${ROOT}/config
 POND=${ROOT}/pond
 
 podman run -ti --rm \
        -v "${POND}:/pond" \
        -v "${OUTDIR}:/data" \
+       -v "${CONFIG}:/config" \
        -e POND=/pond \
        "${IMAGE}" "$@"
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,7 +5,6 @@ SCRIPTS=$(cd "$(dirname "$0")" && pwd)
 ROOT=$(dirname "${SCRIPTS}")
 
 EXE=${ROOT}/scripts/pond.sh
-CONFIG=${ROOT}/config
 
 mkdir pond || (echo "pond already exists"; exit 1)
 
@@ -13,18 +12,18 @@ ${EXE} init
 
 ${EXE} mkdir -p /etc/system.d
 
-${EXE} copy ${CONFIG}/data.md.tmpl /etc
+${EXE} copy /config/data.md.tmpl /etc
 
-${EXE} mknod remote /etc/system.d/backup --config-path ${CONFIG}/backup.yaml
+${EXE} mknod remote /etc/system.d/backup --config-path /config/backup.yaml
 
-${EXE} mknod hydrovu /etc/hydrovu --config-path ${CONFIG}/hydrovu.yaml
+${EXE} mknod hydrovu /etc/hydrovu --config-path /config/hydrovu.yaml
 
-${EXE} mknod column-rename /etc/hydro_rename --config-path ${CONFIG}/hrename.yaml
+${EXE} mknod column-rename /etc/hydro_rename --config-path /config/hrename.yaml
 
-${EXE} mknod dynamic-dir /combined --config-path ${CONFIG}/combine.yaml
+${EXE} mknod dynamic-dir /combined --config-path /config/combine.yaml
 
-${EXE} mknod dynamic-dir /singled --config-path ${CONFIG}/single.yaml
+${EXE} mknod dynamic-dir /singled --config-path /config/single.yaml
 
-${EXE} mknod dynamic-dir /reduced --config-path ${CONFIG}/reduce.yaml
+${EXE} mknod dynamic-dir /reduced --config-path /config/reduce.yaml
 
-${EXE} mknod dynamic-dir /templates --config-path ${CONFIG}/template.yaml
+${EXE} mknod dynamic-dir /templates --config-path /config/template.yaml
